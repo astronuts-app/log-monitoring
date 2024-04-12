3c4ad862c5cd26fb37df6edbc46edc4df51b0e05
Arun Patra <arun@astronuts.io>
2024-04-12 16:39:46 +0530

    Polish

--- log-monitoring-logback/src/main/java/io/astronuts/monitoring/logback/AstronutsAppender.java
+++ log-monitoring-logback/src/main/java/io/astronuts/monitoring/logback/AstronutsAppender.java
@@ -11,16 +11,14 @@ import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.core5.http.io.entity.EntityUtils;
 import org.apache.hc.core5.http.io.entity.StringEntity;
 
-import java.io.IOException;
-
 @SuppressWarnings("unused")
 public class AstronutsAppender<E> extends AsyncAstronutsAppenderBase<E> {
 
-    private static final String DEFAULT_ENDPOINT_URL = "http://localhost:9245/api/foxbat/log-event";
+    private static final String DEFAULT_ENDPOINT_URL = "https://api.astronuts.io/helios/api/foxbat/log-event";
     private final EventTransformer eventTransformer = new DefaultEventTransformer();
     private String secretKey;
     private String endpointUrl;
-
+    private boolean noticeIssued = false;
     @SuppressWarnings("unused")
     public void setSecretKey(String secretKey) {
         this.secretKey = secretKey;
@@ -31,13 +29,19 @@ public class AstronutsAppender<E> extends AsyncAstronutsAppenderBase<E> {
         this.endpointUrl = endpointUrl;
     }
 
+    /**
+     * Check if the event is discardable. By default, events with a level lower than ERROR are discardable.
+     * @param eventObject The event object
+     * @return  true if the event is discardable, false otherwise
+     */
     protected boolean isDiscardable(E eventObject) {
         if (eventObject instanceof ILoggingEvent) {
             ILoggingEvent event = (ILoggingEvent) eventObject;
             Level level = event.getLevel();
             return level.toInt() < Level.ERROR_INT;
+        } else {
+            return false;
         }
-        return false;
     }
 
     @Override
@@ -63,7 +67,8 @@ public class AstronutsAppender<E> extends AsyncAstronutsAppenderBase<E> {
         }
 
         if (secretKey == null || secretKey.trim().isEmpty()) {
-            System.err.println("Error: Astronuts 'File Secret Key' must be provided. Appender will not be started.");
+            System.err.println("Error: Astronuts 'File Secret Key' must be provided. For more information see " +
+                    "https://www.astronuts.io/docs/log-monitoring. After fixing the issue, restart your application.");
             return;
         }
         super.start();
@@ -79,9 +84,8 @@ public class AstronutsAppender<E> extends AsyncAstronutsAppenderBase<E> {
         if (isDiscardable(eventObject)) {
             return;
         }
-        String logMessage = eventTransformer.transformEventToJson(secretKey, new String(encoder.encode(eventObject)));
 
-        sendLogAsync(logMessage);
+        sendLogAsync(eventTransformer.transformEventToJson(secretKey, new String(encoder.encode(eventObject))));
     }
 
     private void sendLogAsync(String logMessage) {
@@ -93,15 +97,37 @@ public class AstronutsAppender<E> extends AsyncAstronutsAppenderBase<E> {
 
                 httpClient.execute(httpPost, response -> {
                     int status = response.getCode();
-                    if (status >= 200 && status < 300) {
-                        return EntityUtils.toString(response.getEntity());
-                    } else {
-                        throw new ClientProtocolException("Unexpected response status: " + status);
+
+                    switch (status) {
+                        case 401:
+                        case 403: {
+                            System.err.println("Error: Unauthorized. For help, please see " +
+                                    "https://www.astronuts.io/docs/log-monitoring. After fixing the issue, " +
+                                    "restart your application.");
+                            super.stop();
+                            break;
+                        }
+                        case 500:
+                            if (!noticeIssued) {
+                                System.err.println("Warning: Astronuts log monitoring is currently unavailable. " +
+                                        "Please check https://status.astronuts.io for service status. Log monitoring" +
+                                        " will be disabled until the service is restored.");
+                                noticeIssued = true;
+                            }
+                            break;
+                        case 200:
+                        case 201:
+                        case 202:
+                            return EntityUtils.toString(response.getEntity());
+                        default: {
+                            super.stop();
+                            throw new ClientProtocolException("Unexpected response status: " + status);
+                        }
                     }
+                    return null;
                 });
-            } catch (IOException e) {
-                // Handle exceptions appropriately
-                //System.out.println("Failed to send log message to Astronuts: " + e.getMessage()) ;
+            } catch (Exception e) {
+                // NOOP
             }
         }).start();
     }
